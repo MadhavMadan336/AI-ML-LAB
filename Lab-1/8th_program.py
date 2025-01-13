7943b999949eb2dc79239dca0dfaeebb3d4979a0
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-01-13 20:58:48 +0530

    Create 8th_program.py

---
+++ Lab-1/8th_program.py
@@ -0,0 +1,8 @@
+# Define the list of numbers
+numbers = [34, 78, 12, 90, 5, 67, 88, 21]
+
+# Find the largest and smallest number
+largest_number = max(numbers)
+smallest_number = min(numbers)
+print(f"The largest number in the list is: {largest_number}")
+print(f"The smallest number in the list is: {smallest_number}")
