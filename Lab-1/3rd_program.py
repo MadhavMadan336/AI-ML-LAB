9cfebbc6b93c0a9137408c1b462852cbfe0f3ad2
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-01-13 20:56:39 +0530

    Create 3rd_program.py

---
+++ Lab-1/3rd_program.py
@@ -0,0 +1,6 @@
+name = input("Enter your name here: ")
+print(f"Hello, {name}! Nice to meet you!")
+dob = int(input("Enter your year of birth: "))
+current_year = 2024
+age = current_year - dob
+print(f"Currently, your age is: {age}")
