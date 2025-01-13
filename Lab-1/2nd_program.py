efeb41b59eed7a1a81e368294621612346c9d045
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2025-01-13 20:56:01 +0530

    Create 2nd_program.py

---
+++ Lab-1/2nd_program.py
@@ -0,0 +1,33 @@
+# Assign values to variables
+integer = 10
+float = 50.0
+string = "Hello"
+boolean = True
+
+# Perform arithmetic operations on numeric data types
+addition = integer + float
+subtraction = integer - float
+multiplication = integer * float
+division = integer / float
+modulus = integer % float
+
+# Print the results
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Modulus: {modulus}")
+
+# Concatenate strings using the + operator
+concatenated_string = string + " World!!"
+print(concatenated_string)
+
+# Use logical operators
+and_op = boolean and False
+or_op = boolean or False
+not_op = not boolean
+
+# Print logical operations
+print(and_op)  # False
+print(or_op)   # True
+print(not_op)  # False
